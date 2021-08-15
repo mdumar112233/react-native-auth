81425fa3e0f1b946308c0be3057804ebfe063548
mdumar112233 <mdumarfaruk187326@gmai.com>
2021-08-15 14:03:51 +0600

    update loading screen

--- Components/Screens/HomeScreen.js
+++ Components/Screens/HomeScreen.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import { useState } from 'react'
 import { Button, StyleSheet, Text, View } from 'react-native'
+import firebase from "firebase/app";
+import "firebase/auth";
 
-export default function HomeScreen({route}) {
+export default function HomeScreen({route, navigation}) {
     const email = route.params;
     console.log(email.email);
-    // const [email, setEmail] = useState('');
+
+    const userSignOut = () => {
+        firebase.auth().signOut()
+        .then(() => {
+            navigation.navigate('SignUp')
+        })
+        .catch(err => {
+            console.log(err.message);
+        })
+    }
     return (
         <View style={styles.container}>
             <Text>You LoggedIn with this email {email.email}</Text>
-            <Button title='logged out' />
+            <Button title='logged out' onPress={userSignOut} />
         </View>
     )
 }
